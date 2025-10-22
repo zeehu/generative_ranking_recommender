437111079a1f62b9479cca6a4e8f36ebe605c079
jezeehu <zee_h@qq.com>
2025-10-22 16:41:39 +0800

    fix

--- src/common/train_word2vec.py
+++ src/common/train_word2vec.py
@@ -83,7 +83,7 @@ def train_song_vectors(config: Config):
     output_file = data_config.song_vectors_file
     logger.info(f"Saving {len(model.wv.index_to_key)} song vectors to {output_file}...")
     with open(output_file, 'w', encoding='utf-8', newline='') as f:
-        writer = csv.writer(f, delimiter='\t')
+        writer = csv.writer(f, delimiter=',')
         for song_id in model.wv.index_to_key:
             vector = model.wv[song_id]
             writer.writerow([song_id] + vector.tolist())
